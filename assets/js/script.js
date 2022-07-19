fbf19deb98c7d6b5e9a23ddadcd8cd33ec5edfca
Tmeng <tonymengt@gmail.com>
2022-07-19 18:04:03 -0400

    updated script.js to create new task when button is clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,20 @@
-var buttonEl = document.querySelector("#save-task");
+// var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 
-var creatTaskHandler = function () {
+var creatTaskHandler = function (event) {
+
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = ("task-item");
     listItemEl.textContent = "this is a new task.";
     tasksToDoEl.appendChild(listItemEl);
+
 }
-buttonEl.addEventListener("click", creatTaskHandler);
+formEl.addEventListener("click", creatTaskHandler);
+// buttonEl.addEventListener("click", creatTaskHandler);
 
 
 // var taskItemEl = document.createElement("li");
