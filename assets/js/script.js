0b64d6f82b8d9c66002bebc67c78e2a879edaeee
Tmeng <tonymengt@gmail.com>
2022-07-20 07:55:30 -0400

    allow input and adding input values into the html code when task is submitted

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,17 +3,31 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 
+
 var creatTaskHandler = function (event) {
 
     event.preventDefault();
 
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // create list item
     var listItemEl = document.createElement("li");
-    listItemEl.className = ("task-item");
-    listItemEl.textContent = "this is a new task.";
+    listItemEl.className = "task-item";
+
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    // give it a class name
+    taskInfoEl.classList = "task-info";
+    // add HTML content to div
+    taskInfoEl.innerHTML="<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
+    // add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
 
 }
-formEl.addEventListener("click", creatTaskHandler);
+formEl.addEventListener("submit", creatTaskHandler);
 // buttonEl.addEventListener("click", creatTaskHandler);
 
 
